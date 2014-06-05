47547ef2d6c3cb74226c2f937b71ee8decd122ae
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2014-06-05 17:44:18 -0300

    Inclusão dos esquemas 1-4

---
+++ livro/images/cap2/descartes-mapa.mm
@@ -0,0 +1,49 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1401995208129" ID="ID_1212523609" MODIFIED="1401997532707">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Descartes</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1401997189802" ID="ID_718696816" MODIFIED="1401997371546" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#193;reas distintas do ser<br />para estudo do m&#233;todo
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1401997189802" MODIFIED="1401997189802" TEXT="Alma: pensamento (Cogito: penso logo existo)"/>
+<node CREATED="1401997189803" MODIFIED="1401997189803" TEXT="Deus (divina): infinito"/>
+<node CREATED="1401997189804" ID="ID_758875572" MODIFIED="1401997388169">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Corpo: estruturas mentais,<br />extens&#227;o da alma/f&#237;sica
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1401997189804" ID="ID_34736712" MODIFIED="1401997189804" POSITION="right" TEXT="Conhecimento">
+<node CREATED="1401997189805" MODIFIED="1401997189805" TEXT="Deve ser Seguro"/>
+<node CREATED="1401997189805" MODIFIED="1401997189805" TEXT="&#xc9; uma constru&#xe7;&#xe3;o do conhecimento"/>
+<node CREATED="1401997189806" ID="ID_296964204" MODIFIED="1401997189806" TEXT="Aplica a D&#xfa;vida Metaf&#xf3;rica (M&#xe9;todo cient&#xed;fico)"/>
+</node>
+</node>
+</map>
