bc0aa97ff4e2918dd170ba09d40ff90e3cbb052b
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2014-08-22 11:27:03 -0300

    Atualização do mapa de decartes

--- livro/images/cap2/descartes-mapa.mm
+++ livro/images/cap2/descartes-mapa.mm
@@ -1,6 +1,6 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1401995208129" ID="ID_1212523609" MODIFIED="1401997532707">
+<node CREATED="1401995208129" ID="ID_1212523609" MODIFIED="1408716653681" STYLE="fork">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -10,9 +10,9 @@
       <b>Descartes</b>
     </p>
   </body>
-</html>
-</richcontent>
-<node CREATED="1401997189802" ID="ID_718696816" MODIFIED="1401997371546" POSITION="right">
+</html></richcontent>
+<edge WIDTH="2"/>
+<node CREATED="1401997189802" ID="ID_718696816" MODIFIED="1408716627435" POSITION="right">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -22,11 +22,10 @@
       &#193;reas distintas do ser<br />para estudo do m&#233;todo
     </p>
   </body>
-</html>
-</richcontent>
-<node CREATED="1401997189802" MODIFIED="1401997189802" TEXT="Alma: pensamento (Cogito: penso logo existo)"/>
-<node CREATED="1401997189803" MODIFIED="1401997189803" TEXT="Deus (divina): infinito"/>
-<node CREATED="1401997189804" ID="ID_758875572" MODIFIED="1401997388169">
+</html></richcontent>
+<node CREATED="1401997189802" MODIFIED="1408716627436" TEXT="Alma: pensamento (Cogito: penso logo existo)"/>
+<node CREATED="1401997189803" MODIFIED="1408716627436" TEXT="Deus (divina): infinito"/>
+<node CREATED="1401997189804" ID="ID_758875572" MODIFIED="1408716627436">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -36,14 +35,12 @@
       Corpo: estruturas mentais,<br />extens&#227;o da alma/f&#237;sica
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
-<node CREATED="1401997189804" ID="ID_34736712" MODIFIED="1401997189804" POSITION="right" TEXT="Conhecimento">
-<node CREATED="1401997189805" MODIFIED="1401997189805" TEXT="Deve ser Seguro"/>
-<node CREATED="1401997189805" MODIFIED="1401997189805" TEXT="&#xc9; uma constru&#xe7;&#xe3;o do conhecimento"/>
-<node CREATED="1401997189806" ID="ID_296964204" MODIFIED="1401997189806" TEXT="Aplica a D&#xfa;vida Metaf&#xf3;rica (M&#xe9;todo cient&#xed;fico)"/>
+<node CREATED="1401997189804" ID="ID_34736712" MODIFIED="1408716627436" POSITION="right" TEXT="Conhecimento">
+<node CREATED="1401997189805" MODIFIED="1408716627436" TEXT="Deve ser Seguro"/>
+<node CREATED="1401997189806" ID="ID_296964204" MODIFIED="1408716627437" TEXT="Aplica a D&#xfa;vida Metaf&#xf3;rica (M&#xe9;todo cient&#xed;fico)"/>
 </node>
 </node>
 </map>
