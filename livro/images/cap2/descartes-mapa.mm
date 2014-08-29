b5cd524068c1598d069260d58c8cd42caed93a3f
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2014-08-29 10:10:56 -0300

    Remoção da numeração dos quadros e atualização do mapa de descartes

--- livro/images/cap2/descartes-mapa.mm
+++ livro/images/cap2/descartes-mapa.mm
@@ -1,7 +1,8 @@
-<map version="0.9.0">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1401995208129" ID="ID_1212523609" MODIFIED="1408716653681" STYLE="fork">
-<richcontent TYPE="NODE"><html>
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node ID="ID_1212523609" CREATED="1401995208129" MODIFIED="1408716653681" STYLE="fork"><richcontent TYPE="NODE">
+
+<html>
   <head>
     
   </head>
@@ -10,37 +11,66 @@
       <b>Descartes</b>
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
 <edge WIDTH="2"/>
-<node CREATED="1401997189802" ID="ID_718696816" MODIFIED="1408716627435" POSITION="right">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      &#193;reas distintas do ser<br />para estudo do m&#233;todo
-    </p>
-  </body>
-</html></richcontent>
-<node CREATED="1401997189802" MODIFIED="1408716627436" TEXT="Alma: pensamento (Cogito: penso logo existo)"/>
-<node CREATED="1401997189803" MODIFIED="1408716627436" TEXT="Deus (divina): infinito"/>
-<node CREATED="1401997189804" ID="ID_758875572" MODIFIED="1408716627436">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      Corpo: estruturas mentais,<br />extens&#227;o da alma/f&#237;sica
-    </p>
-  </body>
-</html></richcontent>
+<hook NAME="MapStyle" zoom="2.0">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="&#xc1;reas distintas do ser&#xa;para estudo do m&#xe9;todo" POSITION="right" ID="ID_230512049" CREATED="1409315188868" MODIFIED="1409315476226">
+<node TEXT="Alma: pensamento (Cogito: penso logo existo)" ID="ID_1036163106" CREATED="1409315188868" MODIFIED="1409315188868"/>
+<node TEXT="Deus (divina): infinito" ID="ID_1965602411" CREATED="1409315188868" MODIFIED="1409315188868"/>
+<node TEXT="Corpo: estruturas mentais,&#xa;extens&#xe3;o da alma/f&#xed;sica" ID="ID_1064930083" CREATED="1409315188868" MODIFIED="1409315486707"/>
 </node>
-</node>
-<node CREATED="1401997189804" ID="ID_34736712" MODIFIED="1408716627436" POSITION="right" TEXT="Conhecimento">
-<node CREATED="1401997189805" MODIFIED="1408716627436" TEXT="Deve ser Seguro"/>
-<node CREATED="1401997189806" ID="ID_296964204" MODIFIED="1408716627437" TEXT="Aplica a D&#xfa;vida Metaf&#xf3;rica (M&#xe9;todo cient&#xed;fico)"/>
+<node TEXT="Conhecimento" POSITION="right" ID="ID_749353950" CREATED="1409315188868" MODIFIED="1409315188868">
+<node TEXT="Deve ser Seguro" ID="ID_231848422" CREATED="1409315188870" MODIFIED="1409315188870"/>
+<node TEXT="Aplica a D&#xfa;vida Metaf&#xf3;rica (M&#xe9;todo cient&#xed;fico)" ID="ID_1642732431" CREATED="1409315188870" MODIFIED="1409315188870"/>
 </node>
 </node>
 </map>
